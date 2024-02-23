0343e49eaba97e7ede5e80b9ecbda573fb6a7dfe
Loïc Gestin <loic.gestin34@gmail.com>
2024-02-23 20:15:01 +0100

    API more intgration FRONT

--- frontend/src/components/organisms/SummonersInfo/SingleMatch.tsx
+++ frontend/src/components/organisms/SummonersInfo/SingleMatch.tsx
@@ -36,29 +36,66 @@ export default function SingleMatch({ data, id }: Props) {
         }
     }, [data.info.gameCreation]);
 
+    const formatGameDuration = (durationInSeconds : number) => {
+        const minutes = Math.floor(durationInSeconds / 60);
+        const seconds = durationInSeconds % 60;
+        return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+    };
+
+    const kdaCalcul = (kill: number, death: number, assist: number) =>{
+        return ((kill + assist) / (death == 0 ? 1 : death)).toFixed(2);
+    }
     return (
     <div className={`rounded-md px-[1vw] py-[1vh] flex justify-between ${user.win ? 'bg-blue-950' : 'bg-red-950'}`}>
-        <div>
-
-          <p>{queuesData.find(queue => queue.queueId === data.info.queueId ).description}</p>
-          <p>{timeSinceCreation}</p>
-            <p>{user.win ? "WIN" : "LOSS"}</p>
+        <div className={"flex-col"}>
+            <div>
+              <p>{queuesData.find(queue => queue.queueId === data.info.queueId ).description}</p>
+              <p>{timeSinceCreation}</p>
+            </div>
+            <p>{user.win ? "WIN " : "LOSS "} {formatGameDuration(data.info.gameDuration) }</p>
         </div>
-        <div className={"flex"}>
-            <div className={"flex-col m-3 items-end"}>
+        <div>
+            <img className="ml-auto" src={`https://ddragon.leagueoflegends.com/cdn/14.4.1/img/champion/${user.championName}.png`} alt={"Champion Image"} width={72} height={72}/>
+        </div>
+        <div className="text-center">
+            <p>{user.kills} / {user.deaths} / {user.assists}</p>
+            <p>{kdaCalcul(user.kills,user.deaths,user.assists)} KDA</p>
+            <p>{user.totalMinionsKilled} CS</p>
+            <p>{user.visionScore} vision</p>
+            <p>{user.visionWardsBoughtInGame} Control Ward</p>
+        </div>
+        <div className="flex-col ">
+            <div className="flex">
+            <img  src={`https://ddragon.leagueoflegends.com/cdn/14.4.1/img/item/${user.item0}.png`} alt={"Item Image"} width={30} height={30}/>
+            <img  src={`https://ddragon.leagueoflegends.com/cdn/14.4.1/img/item/${user.item1}.png`} alt={"Item Image"} width={30} height={30}/>
+            <img  src={`https://ddragon.leagueoflegends.com/cdn/14.4.1/img/item/${user.item2}.png`} alt={"Item Image"} width={30} height={30}/>
+            </div>
+            <div className="flex">
+            <img  src={`https://ddragon.leagueoflegends.com/cdn/14.4.1/img/item/${user.item3}.png`} alt={"Item Image"} width={30} height={30}/>
+            <img  src={`https://ddragon.leagueoflegends.com/cdn/14.4.1/img/item/${user.item4}.png`} alt={"Item Image"} width={30} height={30}/>
+            <img  src={`https://ddragon.leagueoflegends.com/cdn/14.4.1/img/item/${user.item5}.png`} alt={"Item Image"} width={30} height={30}/>
+            </div>
+            <img  src={`https://ddragon.leagueoflegends.com/cdn/14.4.1/img/item/${user.item6}.png`} alt={"Item Image"} width={30} height={30}/>
+        </div>
+        <div className={"flex items-center"}>
+            <div className={"flex-col "} style={{width : "57.8px"}}>
                 {players.slice(0,5).map((player: any, key: number) => (
                     <div key={key} className={"flex "}>
-                        <Link href={`/summoners/${player.summonerName}`}>{player.summonerName}</Link>
-                        <img className="ml-auto" src={`https://ddragon.leagueoflegends.com/cdn/14.4.1/img/champion/${player.championName}.png`} alt={"Champion Image"} width={25} height={25}/>
+                        <div style={{ fontSize: '10.5px', overflow: "hidden",whiteSpace: "nowrap" ,textOverflow:"ellipsis"}}>
+                        <Link  href={`/summoners/${player.summonerName}`}>{player.summonerName}</Link>
+                        </div>
+                        <img className="ml-auto shrink-0"  src={`https://ddragon.leagueoflegends.com/cdn/14.4.1/img/champion/${player.championName}.png`} alt={"Champion Image"} width={20} height={20}/>
                     </div>
                 ))}
             </div>
 
-            <div className={"flex-col m-3"}>
+            <div className={"flex-col"} style={{width : "57.8px"}}>
                 {players.slice(5,10).map((player: any, key: number) => (
                     <div key={key} className={"flex"}>
-                        <img  src={`https://ddragon.leagueoflegends.com/cdn/14.4.1/img/champion/${player.championName}.png`} alt={"Champion Image"} width={25} height={25}/>
-                        <Link href={`/summoners/${player.summonerName}`}>{player.summonerName}</Link>
+                        <img  src={`https://ddragon.leagueoflegends.com/cdn/14.4.1/img/champion/${player.championName}.png`} alt={"Champion Image"} width={20} height={20}/>
+                        <div style={{ fontSize: '10.5px', overflow: "hidden",whiteSpace: "nowrap",textOverflow:"ellipsis" }}>
+                            <Link  href={`/summoners/${player.summonerName}`}>{player.summonerName}</Link>
+                        </div>
                     </div>
                 ))}
             </div>
--- frontend/src/components/organisms/SummonersInfo/SummonerHeader.tsx
+++ frontend/src/components/organisms/SummonersInfo/SummonerHeader.tsx
@@ -5,10 +5,10 @@ export default function SummonerHeader({ data }: any) {
     let summonerDTO = new SummonerDTO(data);
 
   return (
-    <div className=" rounded-md flex-col bg-slate-800 px-[1.5vw] py-[1.5vh]">
-        <h2>Summoner Info</h2>
-        <p>Summoner Name: {summonerDTO.name} </p>
-        <p>Summoner Level: {summonerDTO.summonerLevel}</p>
+    <div className=" rounded-md flex-col mb-10 px-[1.5vw] py-[1.5vh]">
+        <img className={"float-left mr-3 "} src={`https://ddragon.leagueoflegends.com/cdn/14.4.1/img/profileicon/${summonerDTO.profileIconId}.png`} alt={"Item Image"} width={100} height={100}/>
+        <p style={{fontSize : "36px", fontWeight: "bold"}}> {summonerDTO.name} </p>
+        <p> Level: {summonerDTO.summonerLevel}</p>
     </div>
   );
 }

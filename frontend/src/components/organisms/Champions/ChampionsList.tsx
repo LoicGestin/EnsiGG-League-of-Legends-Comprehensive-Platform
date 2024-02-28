68a9f39a45c20d8dac4b47b4deed7174389644b7
turkiea <turkiea@ensimag.fr>
2024-02-28 23:58:21 +0100

    correct images bugs + dl champ images

--- frontend/src/components/organisms/Champions/ChampionsList.tsx
+++ frontend/src/components/organisms/Champions/ChampionsList.tsx
@@ -4,8 +4,17 @@ import championsData from "../../../../public/assets/champions.json";
 
 const ChampionsList = () => {
   return (
-    <div className="bg-gray-900 p-4 text-center">
-      <h1 className="mb-4 text-2xl text-white">Champions</h1>
+    <div className="bg-gray-900 p-4">
+      <div className="mb-4 flex justify-between">
+        <h1 className="px-8 text-2xl text-white">Champions</h1>
+        <div>
+          <button className=" mr-4 h-10 w-10 bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"></button>
+          <button className="mr-4 h-10 w-10 bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"></button>
+          <button className="mr-4 h-10 w-10 bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"></button>
+          <button className="mr-4 h-10 w-10 bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"></button>
+          <button className="mr-4 h-10 w-10 bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"></button>
+        </div>
+      </div>
       <div className="grid grid-cols-6 gap-4">
         {championsData.map((champion) => (
           <Link key={champion.id} href={`/champions/${champion.id}`}>

f95713095b9327b7376a8b7587030ba50430f134
turkiea <turkiea@ensimag.fr>
2024-02-29 00:49:50 +0100

    add button rank isn't working yet

--- frontend/src/components/organisms/Champions/ChampionsList.tsx
+++ frontend/src/components/organisms/Champions/ChampionsList.tsx
@@ -5,14 +5,24 @@ import championsData from "../../../../public/assets/champions.json";
 const ChampionsList = () => {
   return (
     <div className="bg-gray-900 p-4">
-      <div className="mb-4 flex justify-between">
-        <h1 className="px-8 text-2xl text-white">Champions</h1>
-        <div>
-          <button className=" mr-4 h-10 w-10 bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"></button>
-          <button className="mr-4 h-10 w-10 bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"></button>
-          <button className="mr-4 h-10 w-10 bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"></button>
-          <button className="mr-4 h-10 w-10 bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"></button>
-          <button className="mr-4 h-10 w-10 bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"></button>
+      <div className="mb-16 flex items-end justify-between">
+        <h1 className="px-8 py-8 text-4xl text-white">Champions</h1>
+        <div className="flex items-end">
+          <button className="h-25 w-25 mb-2 mr-4 bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
+            <img src="icons/lanes/Position_Gold-Top.png" alt="Top" />
+          </button>
+          <button className="mb-2 mr-4 h-auto w-auto bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
+            <img src="icons/lanes/Position_Gold-Jungle.png" alt="Jungle" />
+          </button>
+          <button className="mb-2 mr-4 h-auto w-auto bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
+            <img src="icons/lanes/Position_Gold-Mid.png" alt="Mid" />
+          </button>
+          <button className="mb-2 mr-4 h-auto w-auto bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
+            <img src="icons/lanes/Position_Gold-Bot.png" alt="Adc" />
+          </button>
+          <button className="mb-2 mr-4 h-auto w-auto bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
+            <img src="icons/lanes/Position_Gold-Support.png" alt="Sup" />
+          </button>
         </div>
       </div>
       <div className="grid grid-cols-6 gap-4">

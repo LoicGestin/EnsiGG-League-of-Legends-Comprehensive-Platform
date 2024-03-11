1f3ff765d2fa29bdc7bbfa5adc67eef3e5836b74
turkiea <turkiea@ensimag.fr>
2024-03-12 00:59:11 +0100

    add splashes

--- frontend/src/app/champions/[nameUID]/page.tsx
+++ frontend/src/app/champions/[nameUID]/page.tsx
@@ -1,7 +1,5 @@
 import { Searchbar } from "@/components/organisms/Searchbar";
-import { Sidebar } from "@/components/organisms/Sidebar";
 import { AppLayout } from "@/components/templates/AppLayout";
-import { ChampionsLayout } from "@/components/templates/ChampionsLayout";
 import Image from "next/image";
 
 export default function Page({
@@ -9,17 +7,28 @@ export default function Page({
 }: {
   params: { nameUID: string };
 }) {
+  const backgroundImageStyle = {
+    backgroundImage: `url(/icons/splashes/${nameUID}_0.jpg)`,
+    backgroundSize: "cover",
+  };
+
   return (
     <AppLayout>
-      <Searchbar />
-      <div className="absolute left-0 top-0" style={{ left: "177px", top: "232px" }}>
-        <div className="p-4 flex items-center text-white">
-          <div className="mr-4 mt-8">
-            <Image src={`/icons/champions/${nameUID}.png`} alt={nameUID} width={81} height={89} />
+      <div className="flex-row">
+        <Searchbar />
+        <div
+          className="flex items-center justify-center rounded-lg bg-gray-900   py-64"
+          style={backgroundImageStyle}
+        >
+          <div className="relative right-60 top-60">
+            <Image
+              src={`/icons/champions/${nameUID}.png`}
+              alt={nameUID}
+              width={81}
+              height={80}
+            />
+            <div className="z-10 text-xl text-white">{nameUID}</div>
           </div>
-          <div className="text-xl">{nameUID}</div>
-        </div>
-        <div className="bg-gray-900 p-4 rounded" style={{ left: "147px", top: "366px", width: "1176px", height: "623px" }} >
         </div>
       </div>
     </AppLayout>

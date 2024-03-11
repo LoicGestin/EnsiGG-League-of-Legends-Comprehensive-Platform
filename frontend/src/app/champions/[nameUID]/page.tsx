cb971469ce5b5d00b1941537c798d0a1e382186f
turkiea <turkiea@ensimag.fr>
2024-03-11 01:10:39 +0100

    fix: champ icon

--- frontend/src/app/champions/[nameUID]/page.tsx
+++ frontend/src/app/champions/[nameUID]/page.tsx
@@ -1,4 +1,6 @@
 import { Searchbar } from "@/components/organisms/Searchbar";
+import { Sidebar } from "@/components/organisms/Sidebar";
+import { AppLayout } from "@/components/templates/AppLayout";
 import { ChampionsLayout } from "@/components/templates/ChampionsLayout";
 import Image from "next/image";
 
@@ -8,10 +10,20 @@ export default function Page({
   params: { nameUID: string };
 }) {
   return (
-    <div>
-      <div className="mx-auto w-3/4">
-        <Image src={`/icons/champions/${nameUID}.png`} alt={nameUID} width={40} height={40}/>
+    <AppLayout>
+      <div
+        className="absolute left-0 top-0"
+        style={{ left: "177px", top: "232px" }}
+      >
+        <div className="p-4">
+          <Image
+            src={`/icons/champions/${nameUID}.png`}
+            alt={nameUID}
+            width={81}
+            height={89}
+          />
+        </div>
       </div>
-    </div>
+    </AppLayout>
   );
 }
